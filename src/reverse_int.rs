53d66720179434d37fae44abfbc552f5015a52a2
Jack Choi <648074155@qq.com>
2021-06-11 16:21:21 +0800

    Update reverse_int.rs

--- src/reverse_int.rs
+++ src/reverse_int.rs
@@ -16,6 +16,15 @@ pub fn reverse(nums: i32) -> i32 {
     }
     
 }
+//solution2
+    pub fn reverse(x: i32) -> i32 {
+        let x_str = x.abs().to_string().chars().rev().collect::<String>();
+        if let Ok(y) = x_str.parse::<i32>() {
+            x.signum() * y
+        } else {
+            0
+        }
+    }
 fn main() {
     let nums = 0;
     println!("{:?}", reverse(nums));
