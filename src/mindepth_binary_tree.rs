b6b9e1d94875c01b80e5946ac7628ad547ef2c82
Jack Choi <648074155@qq.com>
2021-06-15 16:57:10 +0800

    Create mindepth_binary_tree.rs

---
+++ src/mindepth_binary_tree.rs
@@ -0,0 +1,99 @@
+/*
+Minimum Depth of Binary Tree
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: 2
+Example 2:
+
+Input: root = [2,null,3,null,4,null,5,null,6]
+Output: 5
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 105].
+-1000 <= Node.val <= 1000
+*/
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: TreeLink,
+    pub right: TreeLink,
+}
+pub type TreeLink = Option<Rc<RefCell<TreeNode>>>;
+#[macro_export]
+macro_rules! tree {
+    ($e: expr) => {
+        TreeLink::leaf($e)
+    };
+    ($e: expr, $l: expr, $r: expr) => {
+        TreeLink::branch($e, $l, $r)
+    };
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+pub trait TreeCreator {
+    fn branch(val: i32, left: TreeLink, right: TreeLink) -> TreeLink {
+        Some(Rc::new(RefCell::new(TreeNode { val, left, right})))
+    }
+    fn leaf(val: i32) -> TreeLink {
+        Some(Rc::new(RefCell::new(TreeNode {
+            val,
+            left: None,
+            right: None,
+        })))
+    }
+}
+impl TreeCreator for TreeLink {}
+trait Mindepth {
+    fn min_depth(&self) -> usize;
+}
+impl Mindepth for TreeLink {
+    fn min_depth(&self) -> usize {
+        match self {
+            None => 0,
+            Some(node) => {
+                let node = node.borrow();
+                match (&node.left, &node.right) {
+                    (None, None) => 1,
+                    (Some(_), None) => TreeLink::min_depth(&node.left) + 1,
+                    (None, Some(_)) => TreeLink::min_depth(&node.right) + 1,
+                    (Some(_), Some(_)) => {
+                        usize::min(
+                            TreeLink::min_depth(&node.left),
+                            TreeLink::min_depth(&node.right),
+                        ) + 1
+                    }
+                }
+            }
+        }
+    }
+}
+pub fn min_depth(root: TreeLink) -> i32 {
+    root.min_depth() as i32
+}
+fn main() {
+    let x = tree!(1, None, tree!(2, tree!(3), None));
+    let q = tree!(
+        1,
+        tree!(2, tree!(1), tree!(1)),
+        tree!(2, tree!(1), tree!(1))
+    );
+    let p = tree!(
+        1,
+        tree!(2, tree!(1), tree!(1)),
+        tree!(2, tree!(1), tree!(1))
+    );
+    let nums = vec![-10, -3, 0, 5, 9];
+    println!("{:?}", min_depth(q));
+}
