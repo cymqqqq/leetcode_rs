62972707e8fb3a0e4bed08cf66bac0dc496cf816
Jack Choi <648074155@qq.com>
2021-06-16 08:25:44 +0800

    Create tree_path_sum.rs

---
+++ src/tree_path_sum.rs
@@ -0,0 +1,97 @@
+/*
+Path Sum
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+*/
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: TreeLink,
+    pub right: TreeLink,
+}
+pub type TreeLink = Option<Rc<RefCell<TreeNode>>>;
+#[macro_export]
+macro_rules! tree {
+    ($e: expr) => {
+        TreeLink::leaf($e)
+    };
+    ($e: expr, $l: expr, $r: expr) => {
+        TreeLink::branch($e, $l, $r)
+    };
+}
+use std::cell::RefCell;
+use std::rc::Rc;
+pub trait TreeCreator {
+    fn branch(val: i32, left: TreeLink, right: TreeLink) -> TreeLink {
+        Some(Rc::new(RefCell::new(TreeNode { val, left, right})))
+    }
+    fn leaf(val: i32) -> TreeLink {
+        Some(Rc::new(RefCell::new(TreeNode {
+            val,
+            left: None,
+            right: None,
+        })))
+    }
+}
+impl TreeCreator for TreeLink {}
+trait Pathsum {
+    fn path_sum(&self, sum: i32) -> bool;
+}
+impl Pathsum for TreeLink {
+    fn path_sum(&self, sum: i32) -> bool {
+        if let Some(node) = self {
+            let node = node.borrow();
+            let val = node.val;
+            let left = &node.left;
+            let right = &node.right;
+            if left.is_some() && right.is_some() { sum == val }
+            else { right.path_sum(sum - val) || left.path_sum(sum - val) }
+            
+        } else { false }
+    }
+}
+pub fn path_sum(root: TreeLink, sum: i32) -> bool {
+    root.path_sum(sum)
+}
+///////////////////
+
+fn main() {
+    let x = tree!(1, None, tree!(2, tree!(3), None));
+    let q = tree!(
+        1,
+        tree!(2, tree!(1), tree!(1)),
+        tree!(2, tree!(1), tree!(1))
+    );
+    let p = tree!(
+        1,
+        tree!(2, tree!(1), tree!(1)),
+        tree!(2, tree!(1), tree!(1))
+    );
+    let nums = vec![-10, -3, 0, 5, 9];
+    println!("{:?}", path_sum(q, 3));
+}
