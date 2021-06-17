c312cc48df656a0a867af83c2f90a46c398c0f69
Jack Choi <648074155@qq.com>
2021-06-17 09:08:57 +0800

    Update lib_link_list.rs

--- src/lib_link_list.rs
+++ src/lib_link_list.rs
@@ -78,3 +78,43 @@ pub fn reverse_list(head: ListLink) -> ListLink {
     }
     prev
 }
+////////parlimdrome linklist
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct List {
+    head: ListLink,
+}
+impl List {
+    fn new(head: ListLink) -> Self {
+        List { head }
+    }
+    fn pop(&mut self) -> Option<i32> {
+        if let Some(node) = self.head.take() {
+            self.head = node.next;
+            Some(node.val)
+        } else {
+            None
+        }
+    }
+    fn into_iter(self) -> IntoIter {
+        IntoIter {  list: self }
+    }
+}
+pub struct IntoIter {
+    list: List,
+}
+impl Iterator for IntoIter {
+    type Item = i32;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.pop()
+    }
+}
+pub fn list_is_palindrome(head: ListLink) -> bool {
+    let list = List::new(head);
+    let vec: Vec<i32> = list.into_iter().collect();
+    for (i, &v) in vec.iter().rev().enumerate() {
+        if v != vec[i] {
+            return false;
+        }
+    }
+    true
+}
