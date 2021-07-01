caffc57523144d65f2fe9a3a2ab09ad388a16734
Jack Choi <648074155@qq.com>
2021-07-01 15:34:00 +0800

    Update intersection.rs

--- src/intersection.rs
+++ src/intersection.rs
@@ -22,6 +22,29 @@ pub fn intersection(num1: Vec<i32>, num2: Vec<i32>) -> Vec<i32> {
     let bit = &hs1 & &hs2;
     bit.into_iter().collect()
 }
+pub fn intersectin_II(num1: Vec<i32>, num2: Vec<i32>) -> Vec<i32> {
+    let mut hm1: HashMap<i32, i32> = HashMap::new();
+    let hs1: HashSet<i32> = num1.clone().into_iter().collect();
+    num1.iter().for_each(|&x| {
+        *hm1.entry(x).or_default() += 1;
+    });
+    let mut hm2: HashMap<i32, i32> = HashMap::new();
+    let hs2: HashSet<i32> = num2.clone().into_iter().collect();
+    num2.iter().for_each(|&x| {
+        *hm2.entry(x).or_default() += 1;
+    });
+    let bit = &hs1 & &hs2;
+    let mut res: Vec<i32> = vec![];
+    bit.iter().for_each(|&x| {
+        let c1 = hm1[&x];
+        let c2 = hm2[&x];
+        let min = i32::min(c1, c2);
+        (0..min).for_each(|_| {
+            res.push(x);
+        });
+    });
+    res
+}
 fn main() {
     let num1 = vec![1,2,3,4];
     let num2 = vec![3];
