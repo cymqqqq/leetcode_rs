76e926b57353f890c955951265811bc96b0232c1
Jack Choi <648074155@qq.com>
2021-07-02 22:52:20 +0800

    Update lib_stirng.rs

--- src/lib_stirng.rs
+++ src/lib_stirng.rs
@@ -337,6 +337,49 @@ pub fn find_difference(s: String, t: String) -> char {
     }
     unreachable!()
 }
+/*
+Is Subsequence
+Given a string s and a string t, check if s is subsequence of t.
+
+A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ace" is a subsequence of "abcde" while "aec" is not).
+
+Follow up:
+If there are lots of incoming S, say S1, S2, ... , Sk where k >= 1B, and you want to check one by one to see if T has its subsequence. In this scenario, how would you change your code?
+
+Credits:
+Special thanks to @pbrother for adding this problem and creating all test cases.
+
+ 
+
+Example 1:
+
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false
+ 
+
+Constraints:
+
+0 <= s.length <= 100
+0 <= t.length <= 10^4
+Both strings consists only of lowercase characters.
+*/
+pub fn subsequence(s: String, t: String) -> bool {
+    let mut i = 0;
+    let mut j = 0;
+    while i < s.len() && j < t.len() {
+        if s[i..=i] == t[j..=j] {
+            i += 1;
+            j += 1;
+        } else {
+            j += 1;
+        }
+    }
+    i == s.len()
+}
 /////////////
 fn main() {
     let words: Vec<String> = vec!["practice", "makes", "perfect", "coding", "makes"];
