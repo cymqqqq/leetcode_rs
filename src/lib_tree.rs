ba11320bbf971b3c376b97a1688e30b70e9fa399
Jack Choi <648074155@qq.com>
2021-07-11 15:27:08 +0800

    Update lib_tree.rs

--- src/lib_tree.rs
+++ src/lib_tree.rs
@@ -703,6 +703,44 @@ impl Treetostring for TreeLink {
 pub fn treetostring(root: TreeLink) -> String {
     root.tree_to_string()
 }
+/*
+Merge Two Binary Trees
+You are given two binary trees root1 and root2.
+
+Imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not. You need to merge the two trees into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of the new tree.
+
+Return the merged tree.
+
+Note: The merging process must start from the root nodes of both trees.
+
+ 
+
+Example 1:
+
+
+Input: root1 = [1,3,2,5], root2 = [2,1,3,null,4,null,7]
+Output: [3,4,5,5,4,null,7]
+Example 2:
+
+Input: root1 = [1], root2 = [1,2]
+Output: [2,2]
+*/
+pub fn merge_trees(t1: TreeLink, t2: TreeLink) -> TreeLink {
+    match (t1, t2) {
+        (Some(n1), Some(n2)) => {
+            let mut n1 = n1.borrow_mut();
+            let mut n2 = n2.borrow_mut();
+            tree!(
+                n1.val + n2.val,
+                merge_trees(n1.left.take(), n2.left.take()),
+                merge_trees(n1.right.take(), n2.right.take())
+            )
+        }
+        (None, Some(n2)) => Some(n2),
+        (Some(n1), None) => Some(n1),
+        (None, None) => None,
+    }
+}
 ////////////////
 fn main() {
     let x = tree!(1, None, tree!(2, tree!(3), None));
